a34ea83c73f13213b8856470399100d010727dc5
estebandegetau <estebandegetau@gmail.com>
2024-01-22 09:57:37 -0600

    Test use_data

--- data-raw/enigh/R/03_clean/03_use_data.R
+++ data-raw/enigh/R/03_clean/03_use_data.R
@@ -35,26 +35,35 @@ load(here::here(
 
 #---- Use data -----------------------------------------------------------------
 
-for (data_set in enigh_metadata$data_set) {
-  data_set_i <- data_set
+# for (data_set in enigh_metadata$data_set) {
+#   data_set_i <- data_set
+#
+#   load_pre_clean_data(data_set_i, year = year)
+#
+#   data_i <- get(data_set_i)
+#
+#   name_i <- str_c(data_set_i, "_", year)
+#
+#   assign(name_i, data_i)
+#
+#   save(
+#     version = 3,
+#     list = name_i,
+#     file = here::here("data", str_c(name_i, ".rda")),
+#     compress = "xz"
+#   )
+#
+#   rm(list = c(data_set_i, name_i))
+#
+# }
 
-  load_pre_clean_data(data_set_i, year = year)
+load_pre_clean_data("poblacion", year = year)
 
-  data_i <- get(data_set_i)
+poblacion_2022 <- poblacion
 
-  name_i <- str_c(data_set_i, "_", year)
-
-  assign(name_i, data_i)
-
-  save(
-    version = 3,
-    list = name_i,
-    file = here::here("data", str_c(name_i, ".rda")),
-    compress = "xz"
-  )
-
-  rm(list = c(data_set_i, name_i))
-
-}
+use_data(poblacion_2022,
+         overwrite = T,
+         version = 3,
+         compress = "xz")
 
 tools::add_datalist(here::here(), force = T)
